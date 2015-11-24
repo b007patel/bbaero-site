6896a69da2d5289270bd2ae82e560b0ad6cc7f0a
Bharat Patel <b007patel@yahoo.com>
2015-11-24 01:05:25 -0500

    Added a footer to show what tech used to build site. So far only Comodo PositveSSL certificate is there.

--- web/js/custom.js
+++ web/js/custom.js
@@ -1,3 +1,17 @@
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+var footerHeight = -1;
+
+var setLineHeightToHeight = function () {
+	if ( $( ".footer" ).height() != footerHeight ) {  
+		$( ".footer" ).css( "line-height", $( ".footer" ).css( "height" ) );
+		footerHeight = $( ".footer" ).height();
+	};
+};
+
+$( function () {
+	  $( "[data-toggle='tooltip']" ).tooltip();
+	  setLineHeightToHeight();
+} );
+
+$( window ).resize( function() {
+	setLineHeightToHeight();
+} );
