8c80da5515d239dc0260fb92ddbd04f997125622
Bharat Patel <b007patel@yahoo.com>
2015-11-24 16:38:15 -0500

    Added more logos to footer.

--- web/js/custom.js
+++ web/js/custom.js
@@ -1,17 +1,3 @@
-var footerHeight = -1;
-
-var setLineHeightToHeight = function () {
-	if ( $( ".footer" ).height() != footerHeight ) {  
-		$( ".footer" ).css( "line-height", $( ".footer" ).css( "height" ) );
-		footerHeight = $( ".footer" ).height();
-	};
-};
-
 $( function () {
 	  $( "[data-toggle='tooltip']" ).tooltip();
-	  setLineHeightToHeight();
-} );
-
-$( window ).resize( function() {
-	setLineHeightToHeight();
 } );
