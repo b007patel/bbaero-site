63e46a760b54f7decaa1f7aed1c789403fa8a451
Bharat Patel <b007patel@yahoo.com>
2015-11-24 23:04:11 -0500

    - Darkened background for easier reading - Put the noscript div in the correct spot in the template - Changed a few CSS styles

--- web/js/custom.js
+++ web/js/custom.js
@@ -1,5 +1,5 @@
 $( function () {
-	 $( "[data-toggle='tooltip']" ).tooltip();
-	 $( "nav" ).css( "display", "block");
-	 $( "#main" ).css( "display", "block"); 
+	$( "[data-toggle='tooltip']" ).tooltip();
+	$( "nav" ).css( "display", "block");
+	$( "#main" ).css( "display", "block"); 
 } );
