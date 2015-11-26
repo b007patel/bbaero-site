7caf65d613869a815da8e095f6ec49535ffc6b58
Bharat Patel <b007patel@yahoo.com>
2015-11-26 16:35:49 -0500

    Pages now support two-column display ("newspaper columns"). Had the column contain the headings and the text, instead of making the headings and text separate columns on separate rows.

--- templates/php/main-tmpl.php
+++ templates/php/main-tmpl.php
@@ -7,17 +7,21 @@
 		<div id="home-page">
 			<h2 class="text-center"><?php echo "$heading"; ?></h2>
 			<div class="row">
-				<div class="col-xs-12"><h3><?php echo "$lhs_heading"; ?></h3></div>
-				<div id="lhs" class="col-xs-12">
+				<div id="lhs" class="col-sm-12 col-md-6">
+					<h3><?php echo "$lhs_heading"; ?></h3>
+					<div class="body-text">
 <?php
 	echo "$lhs_html";
 ?>
+					</div>
 				</div>
-				<div class="col-xs-12"><h3><?php echo "$rhs_heading"; ?></h3></div>
-				<div id="rhs" class="col-xs-12">
+				<div id="rhs" class="col-sm-12 col-md-6">
+					<h3><?php echo "$rhs_heading"; ?></h3>
+					<div class="body-text">
 <?php
 	echo "$rhs_html";
 ?>
+					</div>
 				</div>
 			</div>
 			<div class="row">
