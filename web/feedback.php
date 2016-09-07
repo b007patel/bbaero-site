22e4b1e00d0e7859c529a2bec8f054de2dcc43d2
Bharat Patel <b007patel@yahoo.com>
2016-09-07 19:27:16 -0400

    - reformatted feedback.php, since it looked horrible on my BB Q10 device

--- web/feedback.php
+++ web/feedback.php
@@ -48,7 +48,7 @@ if(!empty($_SESSION['freecap_word_hash']) && !empty($_POST['rcword']))
 	} else {
 		$word_ok = "no";
 		$lwr_rcword = strtolower($_POST['rcword']);
-		$sess_lhs = $_SESSION['hash_func']($lwr_rcword)."";
+		$sess_bottom = $_SESSION['hash_func']($lwr_rcword)."";
 		$sess_rhs = $_SESSION['freecap_word_hash']."";
 		$fcword = $_POST['rcword'].'('.$lwr_rcword.')';
 	}
@@ -65,20 +65,20 @@ if(!empty($_SESSION['freecap_word_hash']) && !empty($_POST['rcword']))
 	$t = chr(9);
 
 	$heading = 'Feedback';
-	$lhs_heading = '';
+	$form_heading = '';
 	//$ferr = fopen('phperrs', 'a');
 	//$dt = new DateTime();
 	//fwrite($ferr, $dt->format("Y-M-d, H:i:s").$nl."========".$nl);
 	if ($word_ok!==false) {
 		if ($word_ok=="yes") {
-			//fwrite($ferr, 'for word '.$fcword.' sess lhs == rhs'.$nl);
+			//fwrite($ferr, 'for word '.$fcword.' sess bottom == rhs'.$nl);
 			$_SESSION['email_result'] = 'Y::Your e-mail has been sent.';
 		} else {
 			//fwrite($ferr, 'Wrong! trying to set header...'.$nl);
-			//fwrite($ferr, 'for word '.$fcword.' sess lhs of if '.$nl);
-			//fwrite($ferr, 'is "'.$sess_lhs.'", rhs '.$nl);
+			//fwrite($ferr, 'for word '.$fcword.' sess bottom of if '.$nl);
+			//fwrite($ferr, 'is "'.$sess_bottom.'", rhs '.$nl);
 			//fwrite($ferr, 'is "'.$sess_rhs.'"'.$nl.$nl);
-			$lhs_heading = 'Sorry, the word is wrong, try again.';
+			$form_heading = 'Sorry, the word is wrong, try again.';
 			echo '<script type="text/javascript">',
 				'try {new_freecap();}',
 				'catch(e) {console.log("Whoops! no captcha img!");}',
@@ -87,52 +87,49 @@ if(!empty($_SESSION['freecap_word_hash']) && !empty($_POST['rcword']))
 	} else {
 		//fwrite($ferr, "In else file(s) exists branch..".$nl);
 	}
-	//fwrite($ferr, "lhs head len ".strlen($lhs_heading).$nl);
+	//fwrite($ferr, "form head len ".strlen($form_heading).$nl);
 	//fwrite($ferr, "is email_res empty? ".(empty($_SESSION['email_result'])?'yes':'no')."--".$nl);
-	if (strlen($lhs_heading) < 1 && !empty($_SESSION['email_result'])) {
+	if (strlen($form_heading) < 1 && !empty($_SESSION['email_result'])) {
 		if (substr($_SESSION['email_result'], 0, 3) == 'N::') {
-			$lhs_heading = substr($_SESSION['email_result'], 3);
-			//fwrite($ferr, 'Found N::, lhs_heading is now '.$lhs_heading.$nl);
-			//fwrite($ferr, 'unset after setting lhs head to email sent'.$nl);
+			$form_heading = substr($_SESSION['email_result'], 3);
+			//fwrite($ferr, 'Found N::, form_heading is now '.$form_heading.$nl);
+			//fwrite($ferr, 'unset after setting bottom head to email sent'.$nl);
 			unset($_SESSION['email_result']);
 		}
 	}
 	//fclose($ferr);
+	$lhs_heading = '';
 	$rhs_heading = '';
-	$lhs_html = str_repeat(chr(9), 6).'<div id="formdiv">'.$nl;
-	$lhs_html .= str_repeat(chr(9), 7).'<form id="feedbackform" action="'.$_SERVER['PHP_SELF'].'" method="post">'.$nl;
-	$lhs_html .= <<<'EOD'
-						<ul class="fbfieldlist">
-							<li><label>Your e-mail:</label></li><li><input id="fbemail" name="email" class="fbfield" form="feedbackform"></input></li>
-						</ul>
-						<ul class="fbfieldlist">
-							<li><label>Subject:</label></li><li><input id="fbsubject" name="subject" class="fbfield" form="feedbackform"></input></li>
-						</ul>
-						<ul id="fbtextlist">
-							<li><label id="commentslabel">Comments:</label></li>
-							<li><textarea id="fbtext" name="comment" form="feedbackform"></textarea></li>
-							<li><img src="freecap.php" id="freecap"></img></li>
-							<li>If you can't read the word, <a href="#" onClick="this.blur();new_freecap();return false;">click here</a></li>
-							<li>Word above:<input type="text" name="rcword"></input></li>
-							<li><input type="submit" value="Send comment"></input></li>
-						</ul>
-						</form>
-						</div>
+
+	$lhs_html = <<<'EOD'
 
 EOD;
+
 	$rhs_html = <<<'EOD'
 
-
 EOD;
 
-	$bottom_html = <<<'EOD'
+	$bottom_html = "<h3>".$form_heading."</h3>";
+	$bottom_html .= str_repeat(chr(9), 6).'<div id="form_container">'.$nl;
+	$bottom_html .= str_repeat(chr(9), 7).'<form id="feedbackform" action="'.$_SERVER['PHP_SELF'].'" method="post">'.$nl;
+	$bottom_html .= <<<'EOD'
+							<div><label class="widelabel">Your e-mail:</label><input id="fbemail" name="email" class="fbfield" form="feedbackform"></input></div>
+							<div><label>Subject:</label><input id="fbsubject" name="subject" class="fbfield" form="feedbackform"></input></div>
+							<div><label id="fbcommentslabel">Comments:</label></div>
+							<div><textarea id="fbtext" name="comment" form="feedbackform"></textarea></div>
+							<div><img src="freecap.php" id="freecap"></img></div>
+							<div>If you can't read the word, <a href="#" onClick="this.blur();new_freecap();return false;">click here</a></div>
+							<div>Word above:<input type="text" id="rcword" name="rcword" form="feedbackform"></input></div>
+							<div class="button"><input type="submit" form="feedbackform" value="Send comment"></input></div>
+						</form>
+					</div>
 
 EOD;
 
 	include $_SERVER["DOCUMENT_ROOT"].'/php/main-tmpl.php';
 	
 	//$ferr = fopen('phperrs', 'a');
-	//fwrite($ferr, "Is lhs head empty? '".$lhs_heading."'".$nl);
+	//fwrite($ferr, "Is form head empty? '".$form_heading."'".$nl);
 	if (!empty($_SESSION['email_result'])) {
 		//fwrite($ferr, $t."email_res is ".$_SESSION['email_result'].$nl);
 		if (substr($_SESSION['email_result'], 0, 3) == 'Y::') {
