d47adc84707be25ac953f5f8be0ad2ac35356a78
Bharat Patel <b007patel@yahoo.com>
2016-09-15 07:12:21 -0400

    - when submitting recent changes to runtest.php, forgot to submit changes to   custom.js to support radio button behaviour - also found that BB 10 devices (Playbook, Q10) did not have   testform.suite_type.value set. Added alternate code

--- web/js/custom.js
+++ web/js/custom.js
@@ -23,3 +23,57 @@ function new_freecap() {
         alert("Sorry, cannot autoreload freeCap image\nSubmit the form and a new freeCap will be loaded");
     }
 }
+
+function showWaitAndTest() {
+	// set percent value, always. Set it to 0 for expected passes
+	curr_st_formval = document.testform.suite_type;
+	curr_st_val = curr_st_formval.value;
+	if (curr_st_val === undefined) {
+		curr_st_val = getCurrGBDetailText();
+	}
+	curr_st = curr_st_val.substr('exp_'.length);
+	cust_pct_txt = document.getElementById('cust_pct').value;
+	pct = '0'; //assume all pass desired
+	if (curr_st != 'pass') {
+		curr_st = curr_st.substr(5);
+		if (curr_st == 'cust') {
+			pct = cust_pct_txt;
+		} else {
+			pct = curr_st;
+		}
+	}
+	document.getElementById("fail_pct").value = pct;
+	//alert('https://bbaero.freeddns.org/chemtestpage/TestRunner?pct='+pct+'--end--');
+	// show waiting icon, then start tests
+	document.getElementById("testwait").style.display = 'block';
+	//window.open ('https://bbaero.freeddns.org/chemtestpage/TestRunner?pct='+pct,'_self',false);
+}
+
+function getCurrGBDetailText() {
+	var rb = document.querySelector('input[name = "suite_type"]:checked');
+	var st = rb.value;
+	var re = /exp_/g;
+	return st.replace(re, 'gbt_');
+}
+
+function showIDHideOthers(dispID, container, chClassesOrTag) {
+	var cntr = document.querySelector(container);
+	if (cntr === undefined) {
+		console.log('Container "'+container+'" not found. ' +
+			"Defaulting to document as container.");
+		cntr = document;
+	}
+	var childelems;
+	if (chClassesOrTag.search('\.[A-Za-z]') >= 0) {
+		childelems = cntr.getElementsByClassName(chClassesOrTag.substr(1));
+	} else {
+		childelems = cntr.getElementsByTagName(chClassesOrTag);
+	}
+	for (var curkid = 0; curkid < childelems.length; curkid++) {
+		if (childelems[curkid].id == dispID) {
+			childelems[curkid].style.display = 'inherit';
+		} else {
+			childelems[curkid].style.display = 'none';
+		}
+	}
+}
